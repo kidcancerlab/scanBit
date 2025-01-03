695f6fb85cd4eb99d221cfe864742a94a6b0668e
MVesuviusC <matthewvc1@gmail.com>
2025-01-03 15:11:35 -0500

    Updating how conda env is made to include an env variable

--- inst/make_conda_yml.sh
+++ inst/make_conda_yml.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+set -e
+
 conda create -y -n rrrSNVs_xkcd_1337 python=3.12
 conda activate rrrSNVs_xkcd_1337
+
+conda env config vars set rrrSnvs_version=0.2.0
 
 pip install pysam numpy pandas matplotlib scipy
 

375c68df1e47b1f2a86041ccf1a3974e21466fbb
MVesuviusC <matthewvc1@gmail.com>
2025-04-18 14:48:18 -0400

    Changed how I get data for each cluster from original bam. This eliminates the need for getBarcodesFromBam.py and snp_call_splitbams_template.job Now I'm using samtools instead.

--- R/snp_call.R
+++ R/snp_call.R
@@ -17,8 +17,8 @@
 #' @param ref_fasta The path to the reference fasta file.
 #' @param min_depth The minimum depth to use when calling SNPs. Can be a vector
 #'   of numbers.
-#' @param min_snvs_per_cluster The minimum number of sites that must be covered by
-#'   a cell_group to be included in the tree.
+#' @param min_snvs_per_cluster The minimum number of sites that must be covered
+#'   by a cell_group to be included in the tree.
 #' @param max_prop_missing_at_site The maximum proportion of missing data
 #'   allowed at a site.
 #' @param n_bootstraps The number of bootstraps to use when evaluating grouping
@@ -56,7 +56,7 @@ get_snp_tree <- function(cellid_bam_table,
                          min_depth = 5,
                          min_snvs_per_cluster = 500,
                          max_prop_missing_at_site = 0.9,
-                         n_bootstraps = 1000,
+                         n_bootstraps = 10000,
                          bootstrap_cutoff = 0.99,
                          verbose = TRUE,
                          submit = TRUE,
@@ -82,11 +82,11 @@ get_snp_tree <- function(cellid_bam_table,
 
     bam_files <- unique(cellid_bam_table$bam_file)
 
-    results <-
-        parallel::mclapply(seq_len(length(bam_files)),
-                           mc.cores = length(bam_files),
-                           mc.preschedule = FALSE,
-                           function(i) {
+    parallel::mclapply(
+        seq_len(length(bam_files)),
+        mc.cores = length(bam_files),
+        mc.preschedule = FALSE,
+        function(i) {
             bam_name <- bam_files[i]
 
             sub_cellid_bam_table <-
@@ -97,16 +97,9 @@ get_snp_tree <- function(cellid_bam_table,
             call_snps(
                 cellid_bam_table = sub_cellid_bam_table,
                 bam_to_use = bam_name,
-                bam_out_dir = paste0(
+                bcf_dir = file.path(
                     temp_dir,
-                    "/split_bams_",
-                    i,
-                    "/"
-                ),
-                bcf_dir = paste0(
-                    temp_dir,
-                    "/split_bcfs_",
-                    i
+                    paste0("split_bcfs_", i)
                 ),
                 slurm_base = slurm_base,
                 sbatch_base = sbatch_base,
@@ -126,75 +119,90 @@ get_snp_tree <- function(cellid_bam_table,
     # calculate a distance matrix using my slow python script
     # We do this separately for each min_depth provided
 
-    parallel::mclapply(min_depth,
-                       mc.cores = 100,
-                       mc.preschedule = FALSE,
-                       function(this_min_depth) {
-        merge_bcfs(
-            bcf_in_dir = paste0(temp_dir,
-                                "/split_bcfs_[0-9]*_c",
-                                this_min_depth,
-                                "/"),
-            out_bcf = paste0(output_dir,
-                            "/merged",
-                            output_base_name,
-                            "_c",
-                            this_min_depth,
-                            ".bcf"),
-            submit = submit,
-            slurm_base = paste0(slurm_base, "_merge-%j.out"),
-            sbatch_base = sbatch_base,
-            account = account,
-            temp_dir = temp_dir,
-            cleanup = cleanup,
-            other_sbatch_options = other_sbatch_options
-        )
+    parallel::mclapply(
+        min_depth,
+        mc.cores = 100,
+        mc.preschedule = FALSE,
+        function(this_min_depth) {
+            merge_bcfs(
+                bcf_in_dir = file.path(
+                    temp_dir,
+                    paste0(
+                        "split_bcfs_[0-9]*_c",
+                        this_min_depth
+                    )
+                ),
+                out_bcf = file.path(
+                    output_dir,
+                    paste0(
+                        "merged",
+                        output_base_name,
+                        "_c",
+                        this_min_depth,
+                        ".bcf"
+                    )
+                ),
+                submit = submit,
+                slurm_base = paste0(slurm_base, "_merge-%j.out"),
+                sbatch_base = sbatch_base,
+                account = account,
+                temp_dir = temp_dir,
+                cleanup = cleanup,
+                other_sbatch_options = other_sbatch_options
+            )
     })
 
     # Read in the merged bcf and make a tree for each min_depth
     # Number of cores doesn't matter here, we're just submitting slurm jobs
-    parallel::mclapply(min_depth,
-                       mc.cores = 101,
-                       mc.preschedule = FALSE,
-                       function(this_min_depth) {
-        group_clusters_by_dist(
-            merged_bcf_file =
-                paste0(
-                    output_dir,
-                    "/merged",
-                    output_base_name,
-                    "_c",
-                    this_min_depth,
-                    ".bcf"
-                ),
-            output_file =
-                paste0(
-                    output_dir, "/",
-                    output_base_name,
-                    "_",
-                    this_min_depth,
-                    "_dist.txt"
-                ),
-            min_snvs_per_cluster = min_snvs_per_cluster,
-            max_prop_missing_at_site = max_prop_missing_at_site,
-            n_bootstraps = n_bootstraps,
-            bootstrap_cutoff = bootstrap_cutoff,
-            tree_figure_file =
-                paste0(
-                    output_dir, "/",
-                    output_base_name,
-                    "_",
-                    this_min_depth,
-                    "_tree.png"
-                ),
-            verbose = verbose,
-            sbatch_base = sbatch_base,
-            account = account,
-            slurm_base = slurm_base,
-            temp_dir = temp_dir,
-            submit = submit,
-            other_sbatch_options = other_sbatch_options
-        )
+    parallel::mclapply(
+        min_depth,
+        mc.cores = 101,
+        mc.preschedule = FALSE,
+        function(this_min_depth) {
+            group_clusters_by_dist(
+                merged_bcf_file =
+                    file.path(
+                        output_dir,
+                        paste0(
+                            "merged",
+                            output_base_name,
+                            "_c",
+                            this_min_depth,
+                            ".bcf"
+                        )
+                    ),
+                output_file =
+                    file.path(
+                        output_dir,
+                        paste0(
+                            output_base_name,
+                            "_",
+                            this_min_depth,
+                            "_dist.txt"
+                        )
+                    ),
+                min_snvs_per_cluster = min_snvs_per_cluster,
+                max_prop_missing_at_site = max_prop_missing_at_site,
+                n_bootstraps = n_bootstraps,
+                bootstrap_cutoff = bootstrap_cutoff,
+                tree_figure_file =
+                    file.path(
+                        output_dir,
+                        paste0(
+                            output_base_name,
+                            "_",
+                            this_min_depth,
+                            "_tree.png"
+                        )
+                    ),
+                verbose = verbose,
+                sbatch_base = sbatch_base,
+                account = account,
+                slurm_base = slurm_base,
+                temp_dir = temp_dir,
+                submit = submit,
+                other_sbatch_options = other_sbatch_options
+            )
     })
 
     return(TRUE)
@@ -205,7 +213,6 @@ get_snp_tree <- function(cellid_bam_table,
 #' @inheritParams get_snp_tree
 #' @param cellid_bam_table A table with columns cell_id, cell_group and bam_file
 #' @param bam_to_use The bam file to use
-#' @param bam_out_dir The directory to write the bam files to
 #' @param bcf_dir The directory to write the bcf files to
 #'
 #' @return 0 if the snps were called successfully
@@ -215,9 +222,8 @@ get_snp_tree <- function(cellid_bam_table,
 #' @noRd
 call_snps <- function(cellid_bam_table,
                       bam_to_use,
-                      bam_out_dir,
                       bcf_dir,
-                      slurm_base = paste0(getwd(), "/slurmOut_call-%j.txt"),
+                      slurm_base = file.path(getwd(), "slurmOut_call-%j.txt"),
                       sbatch_base = "sbatch_",
                       account = "gdrobertslab",
                       ploidy,
@@ -227,62 +233,46 @@ call_snps <- function(cellid_bam_table,
                       submit = TRUE,
                       cleanup = TRUE,
                       other_sbatch_options = "") {
-    # Check that the bam and bcf directories exist, and if not, create them
-    if (!dir.exists(bam_out_dir)) {
-        dir.create(bam_out_dir)
+    if (!file.exists(bam_to_use)) {
+        stop("Bam file does not exist: ", bam_to_use)
     }
 
     sbatch_other <- make_sbatch_other_string(other_sbatch_options)
 
-    # write out the cell ids to a file with two columns: cell_id, cell_group
-    cell_file <- paste0(bam_out_dir, "/cell_ids.txt")
-    readr::write_tsv(dplyr::select(cellid_bam_table, cell_barcode, cell_group),
-                     file = cell_file,
-                     col_names = FALSE,
-                     progress = FALSE)
-
-    # call getBarcodesFromBam.py on the bam file and the cell id file by reading
-    # in a template and substituting out the placeholder fields
-    # write the bam files to a subfolder for each source bam
-    py_file <-
-        paste0(find.package("scanBit"),
-               "/exec/getBarcodesFromBam.py")
-
-    replace_tibble_split <-
-        dplyr::tribble(
-            ~find,                      ~replace,
-            "placeholder_account",      account,
-            "placeholder_slurm_out",    paste0(
-                                            temp_dir, "/",
-                                            slurm_base,
-                                            "_splitbams-%j.out"
-                                        ),
-            "placeholder_sbatch_other", sbatch_other,
-            "placeholder_cell_file",    cell_file,
-            "placeholder_bam_file",     bam_to_use,
-            "placeholder_bam_dir",      paste0(bam_out_dir, "/"),
-            "placeholder_py_file",      py_file
+    # write out the cell ids to files with two columns: cell_id, cell_group
+    return_values <-
+        dplyr::select(cellid_bam_table, cell_barcode, cell_group) |>
+        dplyr::group_by(cell_group) |>
+        dplyr::group_split() |>
+        lapply(
+            function(x) {
+                x |>
+                    dplyr::select(cell_barcode) |>
+                    readr::write_tsv(
+                        file = file.path(
+                            temp_dir,
+                            paste0(x$cell_group[1], "_cell_ids.txt")
+                        ),
+                        col_names = FALSE,
+                        progress = FALSE
+                    )
+                return()
+            }
         )
 
-    # We're going to split the bam file and store the output in
-    # bam_out_dir/split_bams/. We'll then call mpileup on each of these
-    # bam_out_dir is passed by get_snp_tree() and is the temp_dir/split_bams_{i}/
-    result <-
-        use_sbatch_template(replace_tibble_split,
-                            "snp_call_splitbams_template.job",
-                            warning_label = "Bam splitting",
-                            submit = submit,
-                            file_dir = temp_dir,
-                            temp_prefix = paste0(sbatch_base, "split_"))
+    cell_files <-
+        file.path(
+            temp_dir,
+            paste0(unique(cellid_bam_table$cell_group), "_cell_ids.txt")
+        )
+
+    array_max <-
+        length(cell_files) - 1
 
     ploidy <- pick_ploidy(ploidy)
 
-    array_max <-
-        list.files(path = bam_out_dir, pattern = ".bam") %>%
-        length() - 1
-
     # Since min_depth can be a vector of unknown length, we are going to loop
-    # through each element and call mpileup on each split_bams folder
+    # through each element and call mpileup on each bam
     # Due to this, we need to append the min_depth used to the output bcf folder
     # Since this is just submitting slurm jobs, we don't need to worry about
     # how many cores we use
@@ -295,14 +285,14 @@ call_snps <- function(cellid_bam_table,
             dplyr::tribble(
                 ~find,                      ~replace,
                 "placeholder_account",      account,
-                "placeholder_slurm_out",    paste0(
-                                                temp_dir, "/",
-                                                slurm_base,
-                                                "_mpileup-%j.out"
+                "placeholder_slurm_out",    file.path(
+                                                temp_dir,
+                                                paste0(slurm_base, "_mpileup-%j.out")
                                             ),
                 "placeholder_sbatch_other", sbatch_other,
                 "placeholder_array_max",    as.character(array_max),
-                "placeholder_bam_dir",      bam_out_dir,
+                "placeholder_bam_file",     bam_to_use,
+                "placeholder_cell_files",   paste(cell_files, collapse = " "),
                 "placeholder_ref_fasta",    ref_fasta,
                 "placeholder_ploidy",       ploidy,
                 "placeholder_bcf_dir",      paste0(
@@ -313,7 +303,7 @@ call_snps <- function(cellid_bam_table,
                 "placeholder_min_depth",    as.character(this_min_depth)
             )
 
-        # Call mpileup on each split_bams folder using a template and substituting
+        # Call mpileup on each bam using a template and substituting
         # out the placeholder fields and index the individual bcf files
         result <-
             use_sbatch_template(replace_tibble_snp,
@@ -323,15 +313,7 @@ call_snps <- function(cellid_bam_table,
                                 file_dir = temp_dir,
                                 temp_prefix = paste0(sbatch_base, "mpileup_"))
         })
-    # Delete contents of the split_bams folder
-    # I should change how I do this, this may be a bit dangerous
-    if (cleanup) {
-        system(paste0(
-            "rm ",
-            bam_out_dir,
-            "*.bam"
-        ))
-    }
+
     return(0)
 }
 
@@ -350,15 +332,21 @@ call_snps <- function(cellid_bam_table,
 pick_ploidy <- function(ploidy) {
     if (file.exists(ploidy)) {
         return(paste("--ploidy-file", ploidy))
-    } else if (file.exists(paste0(find.package("scanBit"),
-                                  "/extdata/",
-                                  ploidy,
-                                  "_ploidy.txt"))) {
-        return(paste0("--ploidy-file ",
-                      find.package("scanBit"),
-                      "/extdata/",
-                      ploidy,
-                      "_ploidy.txt"))
+    } else if (file.exists(file.path(find.package("scanBit"),
+                                     "extdata",
+                                     paste0(ploidy, "_ploidy.txt")
+                                     ))) {
+        return(
+            paste0(
+                "--ploidy-file ",
+                file.path(
+                    find.package("scanBit"),
+                    paste0(
+                        "/extdata/",
+                        ploidy,
+                        "_ploidy.txt"))
+                )
+        )
     } else if (ploidy %in% c("GRCh37", "GRCh38", "X", "Y", "1")) {
         return(paste("--ploidy", ploidy))
     } else {
@@ -395,22 +383,21 @@ merge_bcfs <- function(bcf_in_dir,
         dplyr::tribble(
             ~find,                      ~replace,
             "placeholder_account",      account,
-            "placeholder_slurm_out",    paste0(
-                                            temp_dir, "/",
-                                            slurm_base,
-                                            "_merge-%j.out"
+            "placeholder_slurm_out",    file.path(
+                                            temp_dir,
+                                            paste0(slurm_base, "_merge-%j.out")
                                         ),
             "placeholder_sbatch_other", sbatch_other,
             "placeholder_bcf_out",      out_bcf,
             "placeholder_bcf_dir",      bcf_in_dir
         )
 
-    # Call mpileup on each split_bams folder using a template and substituting
+    # Call mpileup merge using a template and substituting
     # out the placeholder fields and index the individual bcf files
     result <-
         use_sbatch_template(replace_tibble_merge,
                             "snp_call_merge_template.job",
-                            warning_label = "Calling SNPs",
+                            warning_label = "Merging bcfs",
                             submit = submit,
                             file_dir = temp_dir,
                             temp_prefix = paste0(sbatch_base, "merge_"))
@@ -484,8 +471,12 @@ group_clusters_by_dist <- function(
             "placeholder_sbatch_other",         sbatch_other,
             "placeholder_py_script",            py_file,
             "placeholder_bcf_input",            merged_bcf_file,
-            "placeholder_min_snvs",             as.character(min_snvs_per_cluster),
-            "placeholder_max_missing",          as.character(max_prop_missing_at_site),
+            "placeholder_min_snvs",             as.character(
+                                                    min_snvs_per_cluster
+                                                ),
+            "placeholder_max_missing",          as.character(
+                                                    max_prop_missing_at_site
+                                                ),
             "placeholder_n_bootstrap",          as.character(n_bootstraps),
             "placeholder_bootstrap_threshold",  as.character(bootstrap_cutoff),
             "placeholder_fig_file",             tree_figure_file,
@@ -493,7 +484,7 @@ group_clusters_by_dist <- function(
             "placeholder_groups_output",        output_file
         )
 
-    # Call mpileup on each split_bams folder using a template and substituting
+    # Call mpileup on each cell id file using a template and substituting
     # out the placeholder fields and index the individual bcf files
     result <-
         use_sbatch_template(replace_tibble_dist,

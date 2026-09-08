475b93f495c1deeb3aa20d1d196105dac5953e70
MVesuviusC <matthewvc1@gmail.com>
2026-09-08 15:38:36 -0400

    Add scanBit-package.R with initial package structure and namespace imports

---
+++ R/scanBit-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom utils download.file
+#' @importFrom utils read.delim
+#' @importFrom utils unzip
+#' @importFrom utils write.table
+## usethis namespace: end
+NULL

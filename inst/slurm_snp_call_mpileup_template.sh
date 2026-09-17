25b7a7a1e69cfa88946da50cb4588899c4061e51
MVesuviusC <matthewvc1@gmail.com>
2026-09-17 19:04:07 -0400

    Fix shebang and refactor snp calling function in slurm template

--- inst/slurm_snp_call_mpileup_template.sh
+++ inst/slurm_snp_call_mpileup_template.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #SBATCH --output=placeholder_job_log
 #SBATCH --error=placeholder_job_log
 #SBATCH --job-name=snp_call
@@ -14,7 +14,7 @@ placeholder_batch_other
 
 set -e ### stops bash script if line ends with error
 
-conda activate scanBit_xkcd_1337
+placeholder_conda_prefixconda activate scanBit_xkcd_1337
 
 start_time=$(date +%s)
 
@@ -34,11 +34,16 @@ fi
 # each column will have a unique name
 label=${cell_file%%_cell_ids.txt}
 export label=${label##*/}
-export orig_samp_name=$(samtools view -H placeholder_bam_file | grep "SM:" | head -n 1 | perl -ne '/\tSM:(.+?)\t/; print $1')
+export orig_samp_name=$(placeholder_apptainersamtools view -H placeholder_bam_file \
+  | grep 'SM:' \
+  | head -n 1 \
+  | perl -pe 's/.+\tSM://' \
+  | perl -pe 's/\t.*$//'placeholder_end_apptainer)
 
 echo ${label} ${orig_samp_name}
 
-samtools view \
+call_snps() {
+  samtools view \
         -u \
         --threads 5 \
         --tag-file CB:${cell_file} \
@@ -63,7 +68,7 @@ samtools view \
     | bcftools filter \
         --threads 5 \
         -g 10 \
-        -e "FORMAT/DP<placeholder_min_depth" \
+        -e 'FORMAT/DP<placeholder_min_depth' \
         -O u \
     | bcftools view \
         --threads 5 \
@@ -71,9 +76,14 @@ samtools view \
         -O b \
         -o placeholder_bcf_dir/${label}.bcf
 
-bcftools index \
+  bcftools index \
     --threads 5 \
     placeholder_bcf_dir/${label}.bcf
+}
+
+export -f call_snps
+
+placeholder_apptainercall_snpsplaceholder_end_apptainer
 
 end_time=$(date +%s)
 
